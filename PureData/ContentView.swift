51756ba530098caa77668487dffc46aa125348bf
demian <nzhd.demian@gmail.com>
2020-04-27 14:26:44 +0300

    Initial Commit

---
+++ PureData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PureData
+//
+//  Created by Demian on 27.04.2020.
+//  Copyright © 2020 Demian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
